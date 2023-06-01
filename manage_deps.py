6545d96b33d419a0475610a9f4822b439e468c54
Claudio Poli <masterkain@gmail.com>
2023-06-01 12:30:29 +0200

    Revert "refactor(manage_deps.py): encapsulate dependency management logic in a class"
    This reverts commit b6badaecea02c9c12f9e2d60b0abbb46051f4b1f.

--- manage_deps.py
+++ manage_deps.py
@@ -1,61 +1,62 @@
 import toml
 import sys
 
-TOML_FILE_PATH = "chatgpt-retrieval-plugin/pyproject.toml"
-DEPENDENCIES_KEY = "tool.poetry.dependencies"
+
+def remove_unused_dependencies(provider):
+    # Define the dependencies for each provider
+    dependencies = {
+        "pinecone": ["weaviate-client", "pymilvus", "qdrant-client", "redis", "chromadb", "llama-index", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
+        "weaviate": ["pinecone-client", "pymilvus", "qdrant-client", "redis", "chromadb", "llama-index", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
+        "zilliz": ["pinecone-client", "weaviate-client", "qdrant-client", "redis", "chromadb", "llama-index", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
+        "milvus": ["pinecone-client", "weaviate-client", "qdrant-client", "redis", "chromadb", "llama-index", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
+        "qdrant": ["pinecone-client", "weaviate-client", "pymilvus", "redis", "chromadb", "llama-index", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
+        "redis": ["pinecone-client", "weaviate-client", "pymilvus", "qdrant-client", "chromadb", "llama-index", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
+        "llamaindex": ["pinecone-client", "weaviate-client", "pymilvus", "qdrant-client", "chromadb", "redis", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
+        "chroma": ["pinecone-client", "weaviate-client", "pymilvus", "qdrant-client", "llama-index", "redis", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
+        "azure": ["pinecone-client", "weaviate-client", "pymilvus", "qdrant-client", "llama-index", "redis", "chromadb", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
+        "supabase": ["pinecone-client", "weaviate-client", "pymilvus", "qdrant-client", "redis", "llama-index", "azure-identity", "azure-search-documents", "psycopg2", "psycopg2cffi", "pgvector"],
+        "postgres": ["pinecone-client", "weaviate-client", "pymilvus", "qdrant-client", "redis", "llama-index", "azure-identity", "azure-search-documents", "supabase"],
+        "analyticdb": ["pinecone-client", "weaviate-client", "pymilvus", "qdrant-client", "redis", "llama-index", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "pgvector"]
+    }
+
+    # Read the pyproject.toml file
+    with open("chatgpt-retrieval-plugin/pyproject.toml", "r") as file:
+        content = toml.load(file)
+
+    # Remove the unused dependencies
+    for dependency in dependencies[provider]:
+        if dependency in content["tool"]["poetry"]["dependencies"]:
+            del content["tool"]["poetry"]["dependencies"][dependency]
+
+    # Write the modified content back to the file
+    with open("chatgpt-retrieval-plugin/pyproject.toml", "w") as file:
+        toml.dump(content, file)
 
 
-class DependencyManager:
-    def __init__(self, toml_file_path):
-        self.toml_file_path = toml_file_path
-        self.content = self.read_toml_file()
-        self.dependencies = {
-            "pinecone": ["weaviate-client", "pymilvus", "qdrant-client", "redis", "chromadb", "llama-index", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
-            "weaviate": ["pinecone-client", "pymilvus", "qdrant-client", "redis", "chromadb", "llama-index", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
-            "zilliz": ["pinecone-client", "weaviate-client", "qdrant-client", "redis", "chromadb", "llama-index", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
-            "milvus": ["pinecone-client", "weaviate-client", "qdrant-client", "redis", "chromadb", "llama-index", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
-            "qdrant": ["pinecone-client", "weaviate-client", "pymilvus", "redis", "chromadb", "llama-index", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
-            "redis": ["pinecone-client", "weaviate-client", "pymilvus", "qdrant-client", "chromadb", "llama-index", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
-            "llamaindex": ["pinecone-client", "weaviate-client", "pymilvus", "qdrant-client", "chromadb", "redis", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
-            "chroma": ["pinecone-client", "weaviate-client", "pymilvus", "qdrant-client", "llama-index", "redis", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
-            "azure": ["pinecone-client", "weaviate-client", "pymilvus", "qdrant-client", "llama-index", "redis", "chromadb", "supabase", "psycopg2", "psycopg2cffi", "pgvector"],
-            "supabase": ["pinecone-client", "weaviate-client", "pymilvus", "qdrant-client", "redis", "llama-index", "azure-identity", "azure-search-documents", "psycopg2", "psycopg2cffi", "pgvector"],
-            "postgres": ["pinecone-client", "weaviate-client", "pymilvus", "qdrant-client", "redis", "llama-index", "azure-identity", "azure-search-documents", "supabase"],
-            "analyticdb": ["pinecone-client", "weaviate-client", "pymilvus", "qdrant-client", "redis", "llama-index", "azure-identity", "azure-search-documents", "supabase", "psycopg2", "pgvector"]
-        }
+def add_dependency_if_not_present(package, version):
+    # Read the pyproject.toml file
+    with open("chatgpt-retrieval-plugin/pyproject.toml", "r") as file:
+        content = toml.load(file)
 
-    def read_toml_file(self):
-        with open(self.toml_file_path, "r") as file:
-            return toml.load(file)
+    # Check if the package is already present in the dependencies
+    if "dependencies" not in content["tool"]["poetry"]:
+        content["tool"]["poetry"]["dependencies"] = {}
 
-    def write_toml_file(self):
-        with open(self.toml_file_path, "w") as file:
-            toml.dump(self.content, file)
+    dependencies = content["tool"]["poetry"]["dependencies"]
+    if package not in dependencies:
+        dependencies[package] = version
 
-    def remove_unused_dependencies(self, provider):
-        for dependency in self.dependencies[provider]:
-            if dependency in self.content[DEPENDENCIES_KEY]:
-                del self.content[DEPENDENCIES_KEY][dependency]
-
-    def add_dependency_if_not_present(self, package, version):
-        if package not in self.content[DEPENDENCIES_KEY]:
-            self.content[DEPENDENCIES_KEY][package] = version
+    # Write the modified content back to the file
+    with open("chatgpt-retrieval-plugin/pyproject.toml", "w") as file:
+        toml.dump(content, file)
 
 
-def main():
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python manage_deps.py <provider>")
         sys.exit(1)
 
     provider = sys.argv[1]
-    manager = DependencyManager(TOML_FILE_PATH)
-    manager.remove_unused_dependencies(provider)
-
+    remove_unused_dependencies(provider)
     # for https://github.com/openai/chatgpt-retrieval-plugin/issues/292#issuecomment-1568588920
-    manager.add_dependency_if_not_present("loguru", ">=0.5.0")
-
-    manager.write_toml_file()
-
-
-if __name__ == "__main__":
-    main()
+    add_dependency_if_not_present("loguru", ">=0.5.0")
